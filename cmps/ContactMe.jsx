1101d0751f33c058762e96eebe377c515a35992e
Elad Tal <eladtal14@gmail.com>
2021-04-03 17:35:40 +0300

    added contact me

--- cmps/ContactMe.jsx
+++ cmps/ContactMe.jsx
@@ -6,11 +6,12 @@ export const ContactMe = () => {
       <h2>Contact me</h2>
       <p>I am available for hire and open to any ideas of cooperation.</p>
       <div className="contact">
-        <span className="right">E-mail: <span className="left"> jacek@jtom.me</span></span>
-        <span className="right">LinkedIn: <span className="left">jtompl</span></span>
-        <span className="right">Github:  <span className="left">jtomaszewski</span></span>
-        <span className="right">Facebook:  <span className="left">jtomaszewski</span></span>
-        <span className="right">Twitter: <span className="left">@jtompl</span></span>
+        <span className="right">E-mail: <span className="left"> <a href="mailto:eladtal14@gmail.com" > Eladtal14@gmail.com </a></span></span>
+
+        <span className="right">LinkedIn: <span className="left"><a href="https://www.linkedin.com/in/elad-tal-0355531b7/" target="_blank"> Elad Tal </a></span></span>
+        <span className="right">Github:  <span className="left"> <a href="https://github.com/EladTal14" target="_blank">EladTal14</a></span></span>
+        <span className="right">Facebook:  <span className="left"> <a href="https://www.facebook.com/elad.tal" target="_blank"> Elad Tal </a></span></span>
+        <span className="right">Twitter: <span className="left"><a href="https://twitter.com/EladTal4" target="_blank">@EladTal4</a> </span></span>
       </div>
     </section>
   )
