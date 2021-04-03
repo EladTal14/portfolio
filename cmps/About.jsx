83ebb13fb472de2c987cb57587c4a896f0e66787
Elad Tal <eladtal14@gmail.com>
2021-04-03 17:22:14 +0300

    added about p and animation

--- cmps/About.jsx
+++ cmps/About.jsx
@@ -4,18 +4,16 @@ export default function About() {
 
   const txtRef = useRef(null)
   useEffect(() => {
-    var newDom = '';
-    var animationDelay = 7;
-
-    for (let i = 0; i < text.innerText.length; i++) {
-      newDom += '<span class="char">' + (text.innerText[i] == ' ' ? '&nbsp;' : text.innerText[i]) + '</span>';
-    }
-
-    text.innerHTML = newDom;
-    var length = text.children.length;
-
+    let animationDelay = 7;
+    let txtWords = txtRef.current.innerText.split(' ')
+    let newTxt = ''
+    txtWords.forEach(word => {
+      newTxt += `<span class="char">${word}</span>&nbsp`
+    })
+    txtRef.current.innerHTML = newTxt
+    let length = txtRef.current.children.length
     for (let i = 0; i < length; i++) {
-      text.children[i].style['animation-delay'] = animationDelay * i + 'ms';
+      txtRef.current.children[i].style['animation-delay'] = animationDelay * i + 'ms'
     }
   }, [])
 
--- cmps/ContactMe.jsx
+++ cmps/ContactMe.jsx
@@ -1,40 +1,17 @@
-import TextField from '@material-ui/core/TextField';
-import { useState } from 'react';
-import SendIcon from '@material-ui/icons/Send';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  button: {
-    margin: theme.spacing(1),
-  },
-}));
 
 export const ContactMe = () => {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const classes = useStyles()
 
-  const sendEmail = (ev) => {
-    ev.preventDefault()
-    console.log();
-  }
   return (
-    <section>
+    <section className="center">
       <h2>Contact me</h2>
-      <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={sendEmail} noValidate autoComplete="off">
-
-        <TextField style={{ marginBottom: 10 }} placeholder="Full name" value={name} name="name" onChange={ev => setName(ev.target.value)} label="Full Name" variant="outlined" />
-        <TextField placeholder="Email" type="email" value={email} name="email" onChange={ev => setEmail(ev.target.value)} label="Email" variant="outlined" />
-        <Button
-          variant="contained"
-          color="primary"
-          className={classes.button}
-          endIcon={<SendIcon></SendIcon>}
-        >
-          Send
-      </Button>
-      </form>
+      <p>I am available for hire and open to any ideas of cooperation.</p>
+      <div className="contact">
+        <span className="right">E-mail: <span className="left"> jacek@jtom.me</span></span>
+        <span className="right">LinkedIn: <span className="left">jtompl</span></span>
+        <span className="right">Github:  <span className="left">jtomaszewski</span></span>
+        <span className="right">Facebook:  <span className="left">jtomaszewski</span></span>
+        <span className="right">Twitter: <span className="left">@jtompl</span></span>
+      </div>
     </section>
   )
 }
