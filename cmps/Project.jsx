048f48d7fde82dc965754a225c46619bf11d55f1
Elad Tal <eladtal14@gmail.com>
2021-04-05 09:49:13 +0300

    test for rebase

--- cmps/Project.jsx
+++ cmps/Project.jsx
@@ -10,16 +10,14 @@ export function Project({ project }) {
       <div className="desc">
         <pre>{desc}</pre>
       </div>
+      <div className="badges">
+        <p>Techonologies:</p>
+        {tags.map(tag => <img key={Math.random()} src={`/images/tags/${tag}.svg`} />)}
+      </div>
       <Image src={imgUrl} width="650" height="400" />
       <div className="buttons">
         <a href={demoUrl} className="myButton" target="_blank">View Demo</a>
         <a href={sourceUrl} className="myButton red" target="_blank">Source Code</a>
-      </div>
-      <div className="badges">
-        <p>Techonologies:</p>
-        {tags.map(tag => <img key={Math.random()} src={`/images/tags/${tag}.svg`} />)}
-
-
       </div>
     </section>
   )
